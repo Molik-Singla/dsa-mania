e7410db9c94431161485a39f779d33852f59f830
Molik Singla <moliksingla02@gmail.com>
2024-06-09 16:05:43 +0530

    add netlify file

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <>
             <header className="flex h-20 w-full items-center justify-between bg-secondary-bg px-4 md:px-8">
-                <p className="text-xl md:text-2xl">Dsa ka Jugaad</p>
+                <p className="text-xl md:text-2xl">Dsa Krle</p>
                 <HeaderButtons />
             </header>
         </>
